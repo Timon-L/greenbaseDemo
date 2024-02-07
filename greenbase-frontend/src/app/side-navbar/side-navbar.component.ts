d65c9555be53b701caae20ab846e28b93295a1ba
timon-l <liuchingchun96@gmail.com>
2024-02-08 09:45:25 +1100

    Added badge for orders in side navbar, added added notifications settings page.

--- greenbase-frontend/src/app/side-navbar/side-navbar.component.ts
+++ greenbase-frontend/src/app/side-navbar/side-navbar.component.ts
@@ -1,22 +1,26 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, Input, OnInit } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { MatSidenavModule } from '@angular/material/sidenav'; 
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { UserData } from '../user-data.model';
+import { UserDataService } from '../user-data.service';
 
 @Component({
   selector: 'app-side-navbar',
   standalone: true,
-  imports: [MatSidenavModule, RouterOutlet, RouterLinkActive, MatListModule, RouterLink, MatIconModule],
+  imports: [MatSidenavModule, RouterOutlet, RouterLinkActive, MatListModule, RouterLink, MatIconModule, MatBadgeModule],
   templateUrl: './side-navbar.component.html',
   styleUrl: './side-navbar.component.scss'
 })
-export class SideNavbarComponent implements AfterViewInit{
+export class SideNavbarComponent implements AfterViewInit, OnInit{
+  userData: UserData = new UserData();
   @ViewChild('drawer') drawer!: MatDrawer;
 
-  constructor(private observer: BreakpointObserver) {}
+  constructor(private observer: BreakpointObserver, private userDataService: UserDataService) {}
 
   ngAfterViewInit() {
     this.observer.observe([Breakpoints.Small, Breakpoints.XSmall]).subscribe(result => {
@@ -28,5 +32,9 @@ export class SideNavbarComponent implements AfterViewInit{
         this.drawer.open();
       }
     });
+  }
+
+  ngOnInit() {
+    this.userDataService.currentUserData.subscribe(data => this.userData = data);
   }
 }
